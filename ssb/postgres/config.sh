64e0e314c493552f07586c8eb43dc2d00bde3ff3
Hakan Memisoglu <hakanmemisoglu@gmail.com>
2016-05-23 18:53:54 -0500

    Postgres files.

---
+++ ssb/postgres/config.sh
@@ -0,0 +1,5 @@
+SSB_TABLE_FILES_DIR="/path/to/ssb/tables"
+
+POSTGRES_EXEC="/path/to/psql"
+
+POSTGRES_DB_NAME="name-of-database"
---
+++ ssb/postgres/run-queries.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+source config.sh
+
+for i in `seq -f "%02g" 1 13`; do
+    echo "QUERY ${i}"
+    for j in `seq 1 5`; do
+        echo "Run ${j}"
+        query_file="${i}.sql"
+        query=`cat "${query_file}"`
+        explain_query="EXPLAIN ANALYZE ${query}"
+        $POSTGRES_EXEC -d $POSTGRES_DB_NAME <<EOF
+	    $explain_query
+EOF
+    done;
+done;
