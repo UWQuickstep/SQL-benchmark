2ba9777c72bccd2ff7f7a9bd19d252618b3d7b22
rogersjeffreyl <rl@cs.wisc.edu>
2016-05-24 11:27:10 -0500

    adding changes for getting the load times

--- ssb/spark/README.md
+++ ssb/spark/README.md
@@ -5,20 +5,32 @@
 
 #### Installation:
 - Download and install scala 2.10.5 from http://www.scala-lang.org/download/2.10.5.html. Any 2.10 version should suffice.
-- Download Spark 1.6.1 http://spark.apache.org/downloads.html. (you do not have to build spark)
 - Install sbt  from http://www.scala-sbt.org/0.13/docs/Setup.html
-- Once that is done copy the files in the conf directory into your spark  configuration directory
-- Edit the  SPARK_LOCAL_DIRS property  to   a directory of  your choice where you are sure to get a lot of disk space. This property can be found in the spark-envs.sh file.
+- Download Spark 1.6.1 http://spark.apache.org/downloads.html. (you do not have to build spark)
+-set the SPARK_HOME environment variable.
+```
+If your spark download is in /home/spark-1.6.1/. The spark home can be set as :
+  export SPARK_HOME=/home/spark-1.6.1
+```
+- Once that is done copy the files in the conf directory into your spark  configuration directory. The spark configuration directory can be found in **$SPARK_HOME/conf**.
+```
+cp -r  conf  $SPARK_HOME/conf
+```
+#### Editing the configuration files:
+```
+cd $SPARK_HOME/conf
+```
+- Edit the  SPARK_LOCAL_DIRS property  to  a directory of  your choice where you are sure to get a lot of disk space. This property can be found in the **spark-envs.sh** file.
 - eg: if your spark  download is at  /u/r/l/rl/Downloads/spark-1.6.1
-this will be your **SPARK_HOME**, then the  configuration  directory would be 
+this will be your **SPARK_HOME**, then the  configuration  directory would be
 $SPARK_HOME/conf.
 
 ### Starting Spark
-- Start the spark master using 
+- Start the spark master using
 ```
 $SPARK_HOME/sbin/start-master.sh
 ```
-This will start the master at port 7077.  
+This will start the master at port 7077.
 The Web dashboard of the master should  be accessible  at port 8080 .
 eg c220g2-011331.wisc.cloudlab.us:8080
 
@@ -40,13 +52,13 @@ Assumming that you have already installed SCALA and sbt
 
 #### Running the Script:
 
-We will be running the scripts using the standard spark submit
-
-SPARK_HOME/sbin/spark-submit
-
+We will be running the scripts using the spark submit script:
+```
+$SPARK_HOME/bin/spark-submit
+```
 eg:
 ```
-bin/spark-submit \
+$SPARK_HOME/bin/spark-submit \
   --class SSB \
   --master spark://node-2.838.quickstep-pg0.wisc.cloudlab.us:7077 \
   --driver-memory 20G \
@@ -62,3 +74,4 @@ bin/spark-submit \
 - --partitions  number of reducers
 - --total-executor-cores  number of executor threads that can run at a time. Controls the number of tasks that can happen in parallel
 
+**Note: THe driver memory as well the the executor memory have to be tuned according to the size of the data set **
--- ssb/spark/src/main/scala/SSB.scala
+++ ssb/spark/src/main/scala/SSB.scala
@@ -152,31 +152,31 @@ object SSB {
         val lineorder =  sc.textFile(f.getPath()).map(_.split("\\|")).map(p => LineOrder(p(0).trim.toInt, p(1).trim.toInt,p(2).trim.toInt,p(3).trim.toInt,p(4).trim.toInt,p(5).trim.toInt,p(6),p(7),p(8).trim.toInt,p(9).trim.toInt,p(10).trim.toInt,p(11).trim.toInt,p(12).trim.toInt,p(13).trim.toInt,p(14).trim.toInt,p(15).trim.toInt,p(16))).toDF()
         lineorder.registerTempTable("lineorder")
         var end = System.currentTimeMillis()
-        file_load_timer_writer.write("lineorder,"+((end-start)))
+        file_load_timer_writer.write("lineorder,"+((end-start))+"\n")
 
         start = System.currentTimeMillis()
         val customer = sc.textFile((new File(ssb_path, "customer.tbl")).getPath()).map(_.split("\\|")).map(p => Customer(p(0).trim.toInt, p(1),p(2),p(3),p(4),p(5),p(6),p(7))).toDF()
         customer.registerTempTable("customer")
         end = System.currentTimeMillis()
-        file_load_timer_writer.write("customer,"+((end-start)))
+        file_load_timer_writer.write("customer,"+((end-start))+"\n")
 
         start = System.currentTimeMillis()
         val supplier = sc.textFile((new File(ssb_path,"supplier.tbl")).getPath()).map(_.split("\\|")).map(p => Supplier(p(0).trim.toInt, p(1),p(2),p(3),p(4),p(5),p(6))).toDF()
         supplier.registerTempTable("supplier")
         end = System.currentTimeMillis()
-        file_load_timer_writer.write("supplier"+((end-start)))
+        file_load_timer_writer.write("supplier"+((end-start))+"\n")
 
         start = System.currentTimeMillis()
         val date = sc.textFile((new File(ssb_path,"date.tbl")).getPath()).map(_.split("\\|")).map(p => Ddate(p(0).trim.toInt, p(1),p(2),p(3),p(4).trim.toInt,p(5).trim.toInt,p(6),p(7).trim.toInt,p(8).trim.toInt,p(9).trim.toInt,p(10).trim.toInt,p(11).trim.toInt,p(12),p(13).trim.toInt,p(14).trim.toInt,p(15).trim.toInt,p(16).trim.toInt)).toDF()
         date.registerTempTable("ddate")
         end = System.currentTimeMillis()
-        file_load_timer_writer.write("ddate:,"+((end-start)))
+        file_load_timer_writer.write("ddate:,"+((end-start))+"\n")
 
         start = System.currentTimeMillis()
         val part = sc.textFile((new File(ssb_path,"part.tbl")).getPath()).map(_.split("\\|")).map(p => Part(p(0).trim.toInt, p(1),p(2),p(3),p(4),p(5),p(6),p(7).trim.toInt,p(8))).toDF()
         part.registerTempTable("part")
         end = System.currentTimeMillis()
-        file_load_timer_writer.write("part,"+((end-start)))
+        file_load_timer_writer.write("part,"+((end-start))+"\n")
 
         file_load_timer_writer.close()
 
@@ -204,7 +204,7 @@ object SSB {
         var query_array= ArrayBuffer[String]()
         if (query_list.length == 0) {
             query_array = queries
-            query_list_name=query_list_name+"+all" 
+            query_list_name=query_list_name+"+all"
         } else {
            for (iter_count <-0 until query_list.length){
              query_array += queries(query_list(iter_count)-1)
