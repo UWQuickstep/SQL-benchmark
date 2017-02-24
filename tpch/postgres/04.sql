29b9f0ca860bef39be9176e73f7d7e298efbf041
Jianqiao Zhu <jianqiao@cs.wisc.edu>
2017-02-24 12:39:12 -0600

    Update 04.sql

--- tpch/postgres/04.sql
+++ tpch/postgres/04.sql
@@ -2,8 +2,7 @@ select
 	o_orderpriority,
 	count(*) as order_count
 from
-	orders,
- 	lineitem
+	orders
 where
 	o_orderdate >= date '1993-07-01'
 	and o_orderdate < date '1993-07-01' + interval '3' month
@@ -19,5 +18,4 @@ where
 group by
 	o_orderpriority
 order by
-	o_orderpriority
-;
+	o_orderpriority;
