f36e00a8168229de0e9d3048c8d399baaf647d68
Hakan Memisoglu <hakanmemisoglu@gmail.com>
2017-02-24 12:18:00 -0600

    Update 04.sql

--- tpch/postgres/04.sql
+++ tpch/postgres/04.sql
@@ -7,8 +7,15 @@ from
 where
 	o_orderdate >= date '1993-07-01'
 	and o_orderdate < date '1993-07-01' + interval '3' month
-	and l_orderkey = o_orderkey
-	and l_commitdate < l_receiptdate
+	and exists (
+		select
+			*
+		from
+			lineitem
+		where
+			l_orderkey = o_orderkey
+			and l_commitdate < l_receiptdate
+	)
 group by
 	o_orderpriority
 order by
