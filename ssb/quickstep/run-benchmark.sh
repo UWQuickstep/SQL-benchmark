60bad05ef42b5056b5e9636c395f6e011bbc0c87
Harshad Deshmukh <hbdeshmukh@apache.org>
2017-03-10 11:39:36 -0600

    Added modified query for flexible DOP experiment.
    - Tweak the predicates to change the hash table sizes.

--- ssb/quickstep/run-benchmark.sh
+++ ssb/quickstep/run-benchmark.sh
@@ -64,7 +64,8 @@ function load_data {
 function run_queries {
   # Runs each SSB query several times.
   QSEXE="$QS $QS_ARGS_BASE $QS_ARGS_NUMA_RUN $QS_ARGS_STORAGE"
-  queries=( 01 02 03 04 05 06 07 08 09 10 11 12 13 )
+  #queries=( 01 02 03 04 05 06 07 08 09 10 11 12 13 )
+  queries=( q07-modified )
   for query in ${queries[@]} ; do
     echo "Query $query.sql"
     rm tmp.sql &>/dev/null
